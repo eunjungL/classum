632c653d21f63395a6ae84977029ac728a23f719
eunjungL <dldmswjd322@gmail.com>
2022-04-03 16:25:27 +0900

    [기능추가] TypeORM 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'dldms',
+        password: 'password',
+        database: 'classum_dev',
+        entities: [],
+        synchronize: false
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
